da24d52e24daab729e9ef84dff9a4083c7ad939c
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-11 03:23:40 -0700

    add cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ import express from "express";
 import notesRouter from "./routes/notes.routes.js"
 import indexrouter from "./routes/index.routers.js"
 
+var cors = require('cors');
+
 const app = express()
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use("/api",express.json())
 
